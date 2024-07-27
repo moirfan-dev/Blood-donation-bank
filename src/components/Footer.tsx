130c5eebd82495e364e0d54314c4efde9abded43
moirfan-dev <mohamedirfanpopz@gmail.com>
2024-07-27 17:14:19 +0530

    added href in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <MDBFooter className='text-center text-lg-end text-muted'>
       <div className='text-center p-4'>
-       <a>
+       <a href='https://github.com/moirfan-dev/Blood-donation-bank'>
          © 2024 Copyright:
         </a>
       </div>
